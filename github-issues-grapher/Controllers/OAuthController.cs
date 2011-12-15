f51792f7dea61181d8ec7df8c4ba3bfd10573868
Tod Karpinski <tkarpinski@gmail.com>
2011-12-14 20:54:20 -0500

    add controllers

---
+++ github-issues-grapher/Controllers/OAuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace github_issues_grapher.Controllers
+{
+    public class OAuthController : Controller
+    {
+        //
+        // GET: /OAuth/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
