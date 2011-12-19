4d54e21f268e58702ff74ff070bbad9604586268
Tod Karpinski <tkarpinski@gmail.com>
2011-12-18 21:32:54 -0500

    add databinding for open issues

--- github-issues-grapher/Scripts/Home.Dashboard.js
+++ github-issues-grapher/Scripts/Home.Dashboard.js
@@ -1,6 +1,6 @@
 ﻿﻿/// <reference path="jquery-1.7.1-vsdoc.js"/>
 /// <reference path="jquery.form.js"/>
-/// <reference path="knockout-1.30beta.debug.js"/>
+/// <reference path="knockout-1.3.0beta.debug.js"/>
 /// <reference path="jquery.linq.js"/>
 /*globals jQuery, window, document*/
 
@@ -21,22 +21,23 @@ var homeDashboard = (function () {
         data: {"access_token" : token }}).done(function(response) {
         vm.name = response.name;
         vm.avatar = response.avatar_url;
-        ko.applyBindings(vm);
+        ko.applyBindings(vm,document.getElementById('nameOnly'));
          });
     })();
     $("#GetIssues").click(function() {
         $.ajax("https://api.github.com/repos/dyknow/dyknowme/issues", {data: {"access_token" : token}}).done(function (response) {
             var vm2 = $(response).toEnumerable();
-            var v3 ={};
             var d7 = new Date();
             d7.setDate(d7.getDate() - 7);
             var d30 = new Date();
             d30.setDate(d30.getDate() - 30);
-            v3.openIssuesCount = vm2.Where(function (x) {return x[0].state === "open"}).Count();
-            v3.closedCount = vm2.Where(function (x) {return x[0].state === "closed"}).Count();
-            v3.last7days = vm2.Where(function (x) {return Date.parse(x[0].created_at) > d7.valueOf()}).Count();
-            v3.last30days = vm2.Where(function (x) {return Date.parse(x[0].created_at) > d30.valueOf()}).Count();
-            v3.activitylast7Days = vm2.Where(function (x) {return Date.parse(x[0].updated_at) > d7.valueOf()}).Count();
+            var vm3 = {};
+            vm3.openIssuesCount =vm2.Where(function (x) {return x[0].state === "open"}).Count();
+            vm3.closedCount=vm2.Where(function (x) {return x[0].state === "closed"}).Count();
+            vm3.last7days=vm2.Where(function (x) {return Date.parse(x[0].created_at) > d7.valueOf()}).Count();
+            vm3.last30days=vm2.Where(function (x) {return Date.parse(x[0].created_at) > d30.valueOf()}).Count();
+            vm3.activitylast7Days=vm2.Where(function (x) {return Date.parse(x[0].updated_at) > d7.valueOf()}).Count();
+            ko.applyBindings(vm3,document.getElementById('openIssuesDiv'));
         });
     
     });
