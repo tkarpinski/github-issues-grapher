2929d192e0cd71517d9485ca873138b67efc20a2
Tod Karpinski <tkarpinski@gmail.com>
2011-12-18 21:47:39 -0500

    look at closed issues now

--- github-issues-grapher/Scripts/Home.Dashboard.js
+++ github-issues-grapher/Scripts/Home.Dashboard.js
@@ -38,6 +38,20 @@ var homeDashboard = (function () {
             vm3.last30days=vm2.Where(function (x) {return Date.parse(x[0].created_at) > d30.valueOf()}).Count();
             vm3.activitylast7Days=vm2.Where(function (x) {return Date.parse(x[0].updated_at) > d7.valueOf()}).Count();
             ko.applyBindings(vm3,document.getElementById('openIssuesDiv'));
+            $('#openIssuesDiv').show();
+        });
+         $.ajax("https://api.github.com/repos/dyknow/dyknowme/issues?state=closed", {data: {"access_token" : token}}).done(function (response) {
+            var vm2 = $(response).toEnumerable();
+            var d7 = new Date();
+            d7.setDate(d7.getDate() - 7);
+            var d30 = new Date();
+            d30.setDate(d30.getDate() - 30);
+            var vm3 = {};
+            vm3.closedCount=vm2.Where(function (x) {return x[0].state === "closed"}).Count();
+            vm3.last7days=vm2.Where(function (x) {return Date.parse(x[0].closed_at) > d7.valueOf()}).Count();
+            vm3.last30days=vm2.Where(function (x) {return Date.parse(x[0].closed_at) > d30.valueOf()}).Count();
+            ko.applyBindings(vm3,document.getElementById('closedIssuesDiv'));
+            $('#closedIssuesDiv').show();
         });
     
     });
